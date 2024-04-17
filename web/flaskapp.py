aa49f77af303aa4720f9b6ccf2f01be671edae7a
ally2211 <amendieta.va@gmail.com>
2024-04-16 21:29:51 -0500

    add flask to first dasboard

--- web/flaskapp.py
+++ web/flaskapp.py
@@ -2,14 +2,14 @@ from flask import Flask
 import dash
 from dash import html, dcc 
 from dashboardsalescall import create_dash_app2 as create_dash_app2
+from fire_dashboard.app9 import create_dash_app1 as create_dash_app1
 
 
 # Initialize Flask app
 flask_app = Flask(__name__)
 
 # Define a route for a simple Flask page
-
-#app1 = create_dash_app1(server)
+app1 = create_dash_app1(flask_app)
 app2 = create_dash_app2(flask_app)
 # Run the Flask server
 if __name__ == '__main__':
